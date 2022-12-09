7c7654d2989728130e2bd7864eda29ab6ebd1d0c
Praga-Dev <pragadeeshwaran.pasupathi@gmail.com>
2022-12-09 18:50:20 +0530

    RepairType Table and Storproc is implemented

---
+++ Database/Praga.PaisaKanaku.Database/Setup/StoredProcedure/RepairType/usp_V1_RepairTypeInfo_Get.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Setup].[usp_V1_RepairTypeInfo_Get]
+	@LoggedInUserId UNIQUEIDENTIFIER
+
+AS
+DECLARE @EmptyGuid UNIQUEIDENTIFIER;
+SET @EmptyGuid = CAST(CAST(0 AS BINARY) AS UNIQUEIDENTIFIER);
+
+BEGIN TRY 
+
+	IF([Auth].[udp_v1_ValidateAccount](@LoggedInUserId) = 0)
+	BEGIN
+		RAISERROR('INVALID_PARAM_LOGGED_IN_USER_ID', 16, 1);
+	END
+
+	SELECT * FROM [Setup].[RepairTypeInfo] WHERE [RowStatus] = 'A' AND [CreatedBy] = @LoggedInUserId ORDER BY [Name];
+
+END TRY  
+BEGIN CATCH  
+	DECLARE @ErrorNumber INT = ERROR_NUMBER();  
+	DECLARE @ErrorMessage NVARCHAR(1000) = ERROR_MESSAGE()   
+     
+	RAISERROR('%s', 16, 1, @ErrorMessage)  
+END CATCH;
