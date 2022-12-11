e8a954214e636a6e519288c2636dc3e925429da1
Chithra <92291415+chithra-m@users.noreply.github.com>
2022-12-11 21:35:09 +0530

    RepairType CRUD Implemented

---
+++ Application/Praga.PaisaKanaku.Core/DataAccess/IRepositories/Setup/IRepairTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Praga.PaisaKanaku.Core.Common.Model;
+using Praga.PaisaKanaku.Core.DataEntities.Setup;
+
+namespace Praga.PaisaKanaku.Core.DataAccess.IRepositories.Setup
+{
+    public interface IRepairTypeRepository
+    {
+        Task<Response<Guid>> SaveRepairTypeInfo(RepairTypeInfoDb repairTypeInfoDb, Guid loggedInUserId);
+        Task<Response<List<RepairTypeInfoDb>>> GetRepairTypeInfoList(Guid loggedInUserId);
+        Task<Response<RepairTypeInfoDb>> GetRepairTypeInfoById(Guid repairTypeInfoId, Guid loggedInUserId);
+    }
+}
