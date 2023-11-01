acc62178faabd54c9c501562086e5a9d2f5374d0
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2023-11-01 13:47:44 +0530

    Create AppConstants.cs

---
+++ Application/Praga.PaisaKanaku.Core/Common/Constants/AppConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Praga.PaisaKanaku.Core.Common.Constants
+{
+    public static class AppConstants
+    {
+        #region Schema
+
+        public const string TRANSACTIONS_SCHEMA = "Transactions";
+
+        #endregion
+
+        #region Table
+
+        public const string EXPENSE_PRODUCT_INFO_TABLE = "ExpenseProductInfo";
+
+        #endregion
+
+    }
+}
