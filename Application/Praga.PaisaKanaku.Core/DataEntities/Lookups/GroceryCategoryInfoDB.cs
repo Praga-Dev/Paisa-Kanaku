d289e91114e4c6f81d247187c7ede51fe93706fa
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2023-11-05 11:36:18 +0530

    Lookups Model added for GroceryInfo

---
+++ Application/Praga.PaisaKanaku.Core/DataEntities/Lookups/GroceryCategoryInfoDB.cs
@@ -0,0 +1,8 @@
+﻿namespace Praga.PaisaKanaku.Core.DataEntities.Lookups
+{
+    internal class GroceryCategoryInfoDB : BaseInfo
+    {
+        public string GroceryCategory { get; set; }
+        public string GroceryCategoryValue { get; set; }
+    }
+}
---
+++ Application/Praga.PaisaKanaku.Core/DomainEntities/Lookups/MetricSystemInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Praga.PaisaKanaku.Core.DomainEntities.Lookups
+{
+    public class MetricSystemInfo
+    {
+        public string MetricSystem { get; set; }
+        public string MetricSystemValue { get; set; }
+    }
+}
