a37b75e9ee975c66ff9b3478d5782eb60cea66b2
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2024-01-03 13:08:11 +0530

    ExpenseOutdoorFood DB implemented

---
+++ Database/Praga.PaisaKanaku.Database/Transactions/StoredProcedure/ExpenseOutdoorFood/usp_ExpenseOutdoorFoodInfo_Get_ByDate.sql
@@ -0,0 +1,64 @@
+﻿CREATE PROCEDURE [Transactions].[usp_ExpenseOutdoorFoodInfo_Get_ByDate]
+	@ExpenseDate DATETIME,
+	@LoggedInUserId UNIQUEIDENTIFIER
+AS
+DECLARE @Response INT = 0;
+
+DECLARE @EmptyGuid UNIQUEIDENTIFIER;
+SET @EmptyGuid = CAST(CAST(0 AS BINARY) AS UNIQUEIDENTIFIER);
+
+BEGIN TRY
+
+	IF([Auth].[udp_v1_ValidateAccount](@LoggedInUserId) = 0)
+	BEGIN
+		RAISERROR('INVALID_PARAM_LOGGED_IN_USER_ID', 16, 1);
+	END
+
+	DECLARE @ExpenseOutdoorFoodInfo TABLE(
+		[Id] UNIQUEIDENTIFIER,
+		[ExpenseDate] DATETIME2,
+		[ExpenseById] UNIQUEIDENTIFIER,
+		[ExpenseByName] NVARCHAR(50),
+		[OutdoorFoodVendorId] UNIQUEIDENTIFIER,
+		[OutdoorFoodVendorName] NVARCHAR(25),
+		[ExpenseAmount] DECIMAL(12,3),
+		[Description] NVARCHAR(250),
+		[CreatedBy] UNIQUEIDENTIFIER,
+		[CreatedDate] DATETIME2,
+		[ModifiedBy] UNIQUEIDENTIFIER,
+		[ModifiedDate] DATETIME2,
+		[RowStatus] NVARCHAR(1)
+	);
+	
+	INSERT INTO @ExpenseOutdoorFoodInfo([Id], [ExpenseDate], [ExpenseById], [ExpenseByName], [OutdoorFoodVendorId], [OutdoorFoodVendorName], [ExpenseAmount], [Description], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [RowStatus])
+	SELECT 
+		[EOFI].[Id],
+		[EOFI].[ExpenseDate],
+		[EOFI].[ExpenseById],
+		[MI].[Name],
+		[EOFI].[OutdoorFoodVendorId],
+		[OFVI].[Name],
+		[EOFI].[ExpenseAmount],
+		[EOFI].[Description],
+		[EOFI].[CreatedBy],
+		[EOFI].[CreatedDate],
+		[EOFI].[ModifiedBy],
+		[EOFI].[ModifiedDate],
+		[EOFI].[RowStatus]	
+	FROM [Transactions].[ExpenseOutdoorFoodInfo] EOFI 
+	LEFT JOIN [Setup].[MemberInfo] MI ON EOFI.[ExpenseById] = MI.[Id]
+	LEFT JOIN [Setup].[OutdoorFoodVendorInfo] OFVI ON EOFI.[OutdoorFoodVendorId] = OFVI.[Id]
+	WHERE [EOFI].[ExpenseDate] = @ExpenseDate 
+	AND [EOFI].[CreatedBy] = @LoggedInUserId
+	AND [EOFI].[RowStatus] = 'A'
+	ORDER BY [MI].[Name]
+
+	SELECT * FROM @ExpenseOutdoorFoodInfo;
+
+END TRY  
+BEGIN CATCH  
+	DECLARE @ErrorNumber INT = ERROR_NUMBER();  
+	DECLARE @ErrorMessage NVARCHAR(1000) = ERROR_MESSAGE()
+     
+	RAISERROR('%s', 16, 1, @ErrorMessage)  
+END CATCH;
---
+++ Database/Praga.PaisaKanaku.Database/Transactions/Table/ExpenseOutdoorFood/ExpenseOutdoorFoodInfo.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [Transactions].[ExpenseOutdoorFoodInfo]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[ExpenseInfoId] UNIQUEIDENTIFIER NOT NULL,
+	[ExpenseDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+	[ExpenseById] UNIQUEIDENTIFIER NOT NULL,
+	[OutdoorFoodVendorId] UNIQUEIDENTIFIER NOT NULL,
+	[ExpenseAmount] DECIMAL(12,3) NOT NULL,
+	[Description] NVARCHAR(250),
+	[CreatedBy] UNIQUEIDENTIFIER NOT NULL,
+	[CreatedDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+	[ModifiedBy] UNIQUEIDENTIFIER NULL,
+	[ModifiedDate] DATETIME2,
+	[RowStatus] NVARCHAR(1) NOT NULL DEFAULT 'A'
+	CONSTRAINT [PK_Transactions_ExpenseOutdoorFoodInfo] PRIMARY KEY NONCLUSTERED
+	(
+		[Id] ASC
+	) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+	CONSTRAINT [FK_ExpenseOutdoorFoodInfo_ExpenseInfo] FOREIGN KEY ([ExpenseInfoId]) REFERENCES [Transactions].[ExpenseInfo]([Id]),
+	CONSTRAINT [FK_ExpenseOutdoorFoodInfo_MemberInfo] FOREIGN KEY ([ExpenseById]) REFERENCES [Setup].[MemberInfo]([Id]),
+	CONSTRAINT [FK_ExpenseOutdoorFoodInfo_OutdoorFoodVendorInfo] FOREIGN KEY ([OutdoorFoodVendorId]) REFERENCES [Setup].[OutdoorFoodVendorInfo]([Id]),
+	CONSTRAINT [FK_ExpenseOutdoorFoodInfo_RowStatus] FOREIGN KEY ([RowStatus]) REFERENCES [Lookups].[RowStatusInfo]([RowStatus])
+) ON [PRIMARY]
+GO
