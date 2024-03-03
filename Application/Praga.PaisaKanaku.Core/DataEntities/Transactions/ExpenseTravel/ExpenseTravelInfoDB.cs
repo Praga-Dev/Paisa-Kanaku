f0652930b1f0ef0462c8884c11d119a78c18fdc1
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2024-03-03 11:40:13 +0530

    Create ExpenseTravelInfoDB.cs

---
+++ Application/Praga.PaisaKanaku.Core/DataEntities/Transactions/ExpenseTravel/ExpenseTravelInfoDB.cs
@@ -0,0 +1,21 @@
+﻿using Praga.PaisaKanaku.Core.Common.Model;
+
+namespace Praga.PaisaKanaku.Core.DataEntities.Transactions.ExpenseTravel
+{
+    public class ExpenseTravelInfoDB : BaseInfo
+    {
+        public Guid Id { get; set; }
+        public Guid ExpenseInfoId { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public Guid ExpenseById { get; set; }
+        public string ExpenseByName { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string Destination { get; set; } = string.Empty;
+        public double ExpenseAmount { get; set; }
+        public string TransportMode { get; set; } = string.Empty;
+        public string TransportModeValue { get; set; } = string.Empty;
+        public string TravelService { get; set; } = string.Empty;
+        public string TravelServiceValue { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
